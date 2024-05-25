904202d4f440a5b43f3f120703a77c957a7343f2
Gleamrise <swk2460050897@outlook.com>
2024-05-25 23:07:02 +0800

    linux 运行脚本

---
+++ exec.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+source /home/ecs-user/project/HotSearchDataAnalysis/venv/bin/activate
+/home/ecs-user/project/HotSearchDataAnalysis/venv/bin/python /home/ecs-user/project/HotSearchDataAnalysis/start-scrape.py
