3b5e6ae72d7c2174aeb09e1f108a4508e76d9c0c
java777 <2206221643@qq.com>
2024-06-03 11:11:06 +0800

    词云

--- src/views/weibo/index.vue
+++ src/views/weibo/index.vue
@@ -1,10 +1,36 @@
 <script setup>
 import HotDataList from "@/components/HotDataList.vue";
-import {ref} from "vue";
+import ChartWordCloud from "@/components/ChartWordCloud.vue";
+import {ref,reactive,onMounted} from "vue";
 const myDate = ref("")
 const getDate = (val)=>{
   myDate.value = val.date
 }
+const state = reactive({
+  chartOptions:{
+    series:[
+      {
+        gridSize: 20,
+        data:[
+          {name: '娜娜米', value: 30},
+          {name: '五条悟',value: 30},
+          { name: '狗卷', value: 28 },
+          { name: 'Shoto', value: 28 },
+          { name: 'Vox', value: 25 },
+          { name: "Aza", value: 23 },
+          { name: 'Mysta', value: 20 },
+          { name: 'Uki', value: 18 },
+          { name: 'Luca', value: 15 },
+          { name: 'Shu', value: 10 },
+          { name: 'Ike', value: 10 },
+          { name: "Fulgun", value: 10 }
+        ]
+      }
+    ]
+  }
+})
+
+onMounted(() => {})
 </script>
 
 <template>
@@ -12,11 +38,16 @@ const getDate = (val)=>{
     weibo
     <HotDataList @sendDate="getDate"/>
   </div>
-
+  <div class="blank" />
+    <ChartWordCloud :options="state.chartOptions" class="chart-cloud"></ChartWordCloud>
 </template>
 
 <style scoped>
 .weibo-common{
   color: white;
+}
+.blank{
+  height: 20px;
+  width: 100%;
 }
 </style>
