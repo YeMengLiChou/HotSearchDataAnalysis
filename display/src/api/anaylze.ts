59526d69af02b74c1a4f79c2ee279c44a98c072c
YeMengLiChou <swk2460050897@outlook.com>
2024-06-06 17:13:29 +0800

    接口

--- display/src/api/anaylze.ts
+++ display/src/api/anaylze.ts
@@ -1,4 +1,3 @@
-import { ApiResult, ApiType, HotSearchOriginData } from './anaylze';
 import { http } from "@/utils/http";
 
 export type ApiResult<T> = {
@@ -58,85 +57,187 @@ export type HotSearchOriginData = {
   data: HotSearchOriginDataItem[];
 };
 
-
 /**
  * 获取热搜的源数据
- * @param apiType 平台类型 
+ * @param apiType 平台类型
  * @param start 开始时间戳
  * @param end 结束时间戳
  * @returns Promise<HotSearchOriginData>
-*/
+ */
 export const getHotSearchOriginData = (
   apiType: number,
   start: number,
   end: number
 ): Promise<ApiResult<HotSearchOriginData>> => {
-  const data = { start, end };
+  const params = { start, end };
 
   if (apiType == ApiType.WeiBoHotSearch) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/weibo", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/weibo", {
+      params
+    });
   }
   if (apiType == ApiType.WeiBoNews) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/weibo-news", {
-      data
-    });
+    return http.request<ApiResult<HotSearchOriginData>>(
+      "get",
+      "/hot/weibo-news",
+      {
+        params
+      }
+    );
   }
   if (apiType == ApiType.WeiBoEntertainment) {
     return http.request<ApiResult<HotSearchOriginData>>(
       "get",
       "/hot/weibo-entertainment",
-      { data }
+      { params }
     );
   }
 
   if (apiType == ApiType.Baidu) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/baidu", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/baidu", {
+      params
+    });
   }
   if (apiType == ApiType.Zhihu) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/zhihu", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/zhihu", {
+      params
+    });
   }
   if (apiType == ApiType.PengPai) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/pengpai", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/pengpai", {
+      params
+    });
   }
   if (apiType == ApiType.TouTiao) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/toutiao", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/toutiao", {
+      params
+    });
   }
   if (apiType == ApiType.Sougou) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/sougou", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/sougou", {
+      params
+    });
   }
   if (apiType == ApiType.Douyin) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/douyin", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/douyin", {
+      params
+    });
   }
   if (apiType == ApiType.Bilibili) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/bilibili", { data });
+    return http.request<ApiResult<HotSearchOriginData>>(
+      "get",
+      "/hot/bilibili",
+      { params }
+    );
   }
   if (apiType == ApiType.KuaiShou) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/kuaishou", { data });
+    return http.request<ApiResult<HotSearchOriginData>>(
+      "get",
+      "/hot/kuaishou",
+      { params }
+    );
   }
   if (apiType == ApiType.TencentNews) {
-    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/tencent", { data });
+    return http.request<ApiResult<HotSearchOriginData>>("get", "/hot/tencent", {
+      params
+    });
   }
   throw Error("apiType doesn't match!");
 };
 
-
-
-export type WordCloudResultItem = {
-  timestamp: number,
+export type WordCloudHotNumItem = {
+  timestamp: number;
   words: {
-    word: string,
-    hot_num: number,
-  }[]
+    word: string;
+    hot_num: number;
+  }[];
 };
 
-
-export const getWordCloud = (
+/**
+ * 获取词云数据，根据热度排序
+ *
+ */
+export const getWordCloudHotNum = (
   apiType: number,
   start: number,
   end: number
-): Promise<ApiResult<WordCloudResultItem[]>> => {
-  const data = {
-    start, end
+): Promise<ApiResult<WordCloudHotNumItem[]>> => {
+  const params = {
+    start,
+    end
   };
-  return http.request<ApiResult<WordCloudResultItem[]>>("get", `/hot/word-cloud-hot-num/${apiType}`, { data });
+  return http.request<ApiResult<WordCloudHotNumItem[]>>(
+    "get",
+    `/hot/word-cloud-hot-num/${apiType}`,
+    { params }
+  );
+};
+
+export type WordCloudNumItem = {
+  timestamp: number;
+  words: {
+    word: string;
+    num: number;
+  }[];
+};
+
+/**
+ * 获取词云数据，根据出现次数排序
+ *
+ */
+export const getWordCloudNum = (
+  apiType: number,
+  start: number,
+  end: number
+): Promise<ApiResult<WordCloudNumItem[]>> => {
+  const params = {
+    start,
+    end
+  };
+  return http.request<ApiResult<WordCloudNumItem[]>>(
+    "get",
+    `/hot/word-cloud-num/${apiType}`,
+    { params }
+  );
+};
+
+export type TrendingDataItem = {
+  title: string;
+  start_timestamp: number;
+  end_timestamp: number;
+  max_hot_num: number;
+  avg_hot_num: number;
+  min_hot_num: number;
+  max_rank: number;
+  min_rank: number;
+  trending_list: {
+    timestamp: number;
+    hot_num: number;
+    rank: number;
+  }[];
+};
+
+/**
+ * 获取变化趋势数据
+ *
+ */
+export const getTrendingData = (
+  apiType: number,
+  start: number,
+  end: number
+): Promise<ApiResult<TrendingDataItem[]>> => {
+  const params = {
+    start,
+    end
+  };
+  return http.request<ApiResult<TrendingDataItem[]>>(
+    "get",
+    `/hot/trending-data/${apiType}`,
+    { params }
+  );
+};
+
+export type WeiBoCategoryData = {
+  category: string;
+  values: {}[];
 };
