b4c7d58d2b37267494b7d29cee59b94b8ace9044
YeMengLiChou <swk2460050897@outlook.com>
2024-06-06 22:46:11 +0800

    界面

--- display/src/components/charts/HotDataList.vue
+++ display/src/components/charts/HotDataList.vue
@@ -5,6 +5,7 @@ import HeatCurveEntry from "@/components/HeatCurveEntry.vue";
 import * as echarts from "echarts/core";
 import { getTrendingData } from "@/api/anaylze";
 import { timestamp } from "@vueuse/core";
+import analyze from "@/router/modules/views/analyze";
 
 const emits = defineEmits(["sendDate"]);
 const size = ref<"default" | "large" | "small">("default");
@@ -125,8 +126,7 @@ function DrawCategory(data: any, val: any) {
   let myChart2 = echarts.init(document.getElementById("-chart-" + val));
   const option2 = {
     xAxis: {
-      type: "category",
-    
+      type: "category"
     },
     yAxis: {
       type: "value",
@@ -183,9 +183,28 @@ const formattedDate = (timestamp: number) => {
 
   return `${year}年${month}月${day}日 ${hours}:${minutes}`;
 };
-const transformWordCloud = (data: any[]) => {
+
+type Item = {
+  timestamp: number;
+  hot_num: number;
+  rank: number;
+};
+
+type TranformedItem = {
+  avg_hot_num: number;
+  min_hot_num: number;
+  max_hot_num: number;
+  min_rank: number;
+  max_rank: number;
+  duration: string;
+};
+
+const analyzedItem = ref<TranformedItem>(null);
+
+const transformWordCloud = (data: Item[]) => {
   const rankItems: Array<rankData> = [];
   const hotItems: Array<hotData> = [];
+
   data.forEach(item => {
     rankItems.push({
       timestamp: formattedDate(item.timestamp),
@@ -205,7 +224,6 @@ const transformWordCloud = (data: any[]) => {
     item.hot_num
   ]);
   return {
-    
     transformedRankData,
     transformedHotData
   };
@@ -231,9 +249,35 @@ const handleCollapseChange = val => {
     )
       .then(res => {
         if (res.data.length > 0) {
-          const trending_list = res.data[0].trending_list;
+          const data = res.data[0];
+          const trending_list = data.trending_list;
 
           let output = transformWordCloud(trending_list);
+
+          let maxTime = 0;
+          let minTime = 999999999999999999;
+          trending_list.forEach(item => {
+            maxTime = Math.max(maxTime, item.timestamp);
+            minTime = Math.min(minTime, item.timestamp);
+          });
+
+          const duration = maxTime - minTime;
+          // 计算总分钟数
+          const totalMinutes = Math.floor(duration / (1000 * 60));
+
+          // 分离出小时和分钟部分
+          const hours = Math.floor(totalMinutes / 60);
+          let minutes = totalMinutes % 60; // 使用取余运算得到剩余的分钟数
+
+          analyzedItem.value = {
+            max_hot_num: data.max_hot_num,
+            min_hot_num: data.min_hot_num,
+            avg_hot_num: data.avg_hot_num,
+            max_rank: data.max_rank,
+            min_rank: data.min_rank,
+            duration: `${hours}小时${minutes}分钟`
+          };
+
           console.log(output);
           DrawCategory(output, val);
         }
@@ -302,6 +346,29 @@ const value1 = ref<[Date, Date]>([curDate, new Date()]);
         :name="i.title + '-' + i.time"
         :key="i.time"
       >
+        <template v-if="analyzedItem != null">
+          <el-descriptions class="margin-top" :column="3" border>
+            <el-descriptions-item label="榜上最高热度">{{
+              analyzedItem.max_hot_num
+            }}</el-descriptions-item>
+            <el-descriptions-item label="榜上最低热度">{{
+              analyzedItem.min_hot_num
+            }}</el-descriptions-item>
+            <el-descriptions-item label="榜上平均热度">{{
+              analyzedItem.avg_hot_num
+            }}</el-descriptions-item>
+            <el-descriptions-item label="榜上最高排名">{{
+              analyzedItem.min_rank
+            }}</el-descriptions-item>
+            <el-descriptions-item label="榜上最低排名">{{
+              analyzedItem.max_rank
+            }}</el-descriptions-item>
+            <el-descriptions-item label="在榜持续时间">{{
+              analyzedItem.duration
+            }}</el-descriptions-item>
+          </el-descriptions>
+        </template>
+
         <template #title>
           <div class="hot-list-item-id">{{ i.rank }}</div>
           <div>{{ i.title }}</div>
